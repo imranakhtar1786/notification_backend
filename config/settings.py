695cfc2f205b79bbab4d1666b401ac4768216381
Imran Akhtar <imranakhtar1786@gmail.com>
2026-09-12 17:55:52 +0530

    allowed host

--- config/settings.py
+++ config/settings.py
@@ -24,7 +24,7 @@ ALLOWED_HOSTS = [
     host.strip()
     for host in os.getenv(
         "ALLOWED_HOSTS",
-        "127.0.0.1,localhost"
+        "127.0.0.1,localhost,.onrender.com"
     ).split(",")
     if host.strip()
 ]
